ac176be04209d12a9913a29d97cd621f2a33da56
鐘梓菱 <a25729308@gmail.com>
2023-05-17 19:48:41 +0800

    喝水建議量-2

--- Web411018947/Controllers/WaterController.cs
+++ Web411018947/Controllers/WaterController.cs
@@ -9,8 +9,16 @@ namespace Web411018947.Controllers
     public class WaterController : Controller
     {
         // GET: Water
-        public ActionResult Index()
+        public ActionResult Index(string name, float? weight)
         {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+
+            ViewBag.name = name;
+            ViewBag.Result = result;
             return View();
         }
     }
